12616784b428285fe648bd50ed84d41a9f10e209
adityamfu <akunpengganti.aditya@gmail.com>
2023-10-24 13:39:58 +0700

    fix login guard

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -19,6 +19,10 @@ class RedirectIfAuthenticated
     {
         $guards = empty($guards) ? [null] : $guards;
 
+        if (Auth::guard('admin')->check()) {
+            return redirect(RouteServiceProvider::HOME);
+        }
+
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
                 return redirect(RouteServiceProvider::HOME);
