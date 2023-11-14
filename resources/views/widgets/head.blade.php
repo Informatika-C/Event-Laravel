f89ae8a027b6c4172b4ff97a55f17aae193c84c6
adityamfu <akunpengganti.aditya@gmail.com>
2023-11-15 00:30:15 +0700

    lomba select content

--- resources/views/widgets/head.blade.php
+++ resources/views/widgets/head.blade.php
@@ -1,7 +1,8 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap" />
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" />
     <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
     <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}" />
@@ -12,10 +13,10 @@
     <link rel="stylesheet" href="{{ asset('assets/css/footer.css') }}" />
     <link rel="stylesheet" href="{{ asset('assets/css/responsive.css') }}" />
     <link rel="stylesheet" href="{{ asset('assets/css/profile.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/css/lomba.css') }}" />
     <script src="{{ asset('assets/js/theme.js') }}" type="module"></script>
-    <script src="{{ asset('assets/js/profile.js') }}" ></script>
-    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
     <script type="module" src="{{ asset('assets/js/animated.js') }}"></script>
+    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/gsap.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/TextPlugin.min.js"></script>
     <link rel="icon" href="{{ asset('assets/images/logo.png') }}" type="image/x-icon">
